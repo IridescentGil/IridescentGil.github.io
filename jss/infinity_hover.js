de3b76bc4ce88dd01d16cc10115ea38d2aabee87
Apile Tyumre <apiletyumre@iridescentsun.com>
2025-06-24 09:57:14 +0200

    Fix blurry image in Chrome Clean up JS Code. Make words in main infinity logo bold

--- jss/infinity_hover.js
+++ jss/infinity_hover.js
@@ -11,6 +11,11 @@ const INFINITY_LOGO_LINK_ADRESS_MAP = new Map([
     ['billing-management', 'https://www.google.com'],
 ],
 );
+// Apply tranform scale to height and with to
+// allow the removal of tranform attribute for consistent placement
+const INFORMATION_BOX_TRANSFORMATION_SCALE = 0.24;
+// Offset to position elements properly
+const INFORMATION_BOX_X_OFFSET = 30;
 
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('infinity-logo-layer').
@@ -21,12 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 displayInformationBox(evt);
             } else if (!evt.target.classList.contains('infinity-link') &&
                 !evt.target.classList.contains('infinity-info')) {
-
-                const infinityinformationLayer =
-                document.getElementById('infinity-information-layer');
-                if (infinityinformationLayer) {
-                    infinityinformationLayer.remove();
-                }
+                document.getElementById('infinity-information-layer')?.remove();
             }
         });
 });
@@ -38,40 +38,25 @@ function displayInformationBox(evt) {
 
     const linkAdress = INFINITY_LOGO_LINK_ADRESS_MAP.get(linkId);
 
-    const infinityInformationLayer = createInformationBox(evt, linkId, linkAdress);
+    const infinityInformationLayer = createInformationBox(evt.target, linkId, linkAdress);
 
     document.getElementById('infinity-logo-layer').append(infinityInformationLayer);
-
-    const animations = document.getElementById('infinity-information-layer')
-    .getElementsByTagName('animate');
-
-    for (let i = 0; i < animations.length; i++) {
-        animations[i].beginElement();
-    }
 }
 
-
-function createInformationBox(evt, linkId, linkAdress) {
+function createInformationBox(target, linkId, linkAdress) {
     const svgns = 'http://www.w3.org/2000/svg';
     const infinityInformationLayer = document.createElementNS(svgns, 'g');
     infinityInformationLayer.setAttribute('id', 'infinity-information-layer');
 
     const infinityLogoTextStyle =
-    window.getComputedStyle(document.getElementById(evt.target.id));
+    window.getComputedStyle(document.getElementById(target.id));
     const xPositionInfinityLogoText =
     parseFloat(infinityLogoTextStyle.getPropertyValue('--x'));
     const yPositionInfinityLogoText =
     parseFloat(infinityLogoTextStyle.getPropertyValue('--y'));
 
-
-    // Apply tranform scale to height and with to
-    // allow the removal of tranform attribute for consistent placement
-    const INFORMATION_BOX_TRANSFORMATION_SCALE = 0.24;
-    // Offset to position elements properly
-    const INFORMATION_BOX_X_OFFSET = 30;
-
     const informationBoxYOffset = yPositionInfinityLogoText + 
-        (parseFloat(evt.target.getAttribute('height') *
+        (parseFloat(target.getAttribute('height') *
             parseFloat(infinityLogoTextStyle.getPropertyValue('--scale'))));
 
     let informationBox = document.getElementById(linkId + '-information')
@@ -80,7 +65,6 @@ function createInformationBox(evt, linkId, linkAdress) {
     informationBox.setAttribute('x',
         xPositionInfinityLogoText - INFORMATION_BOX_X_OFFSET);
     informationBox.setAttribute('y', informationBoxYOffset);
-    informationBox.setAttribute('opacity', '0');
     informationBox.setAttribute('height', informationBox.getAttribute('height') *
         INFORMATION_BOX_TRANSFORMATION_SCALE);
     informationBox.setAttribute('width', informationBox.getAttribute('width') *
@@ -96,24 +80,21 @@ function createInformationBox(evt, linkId, linkAdress) {
         INFORMATION_BOX_TRANSFORMATION_SCALE);
     readMoreButton.setAttribute('x',
         parseFloat(informationBox.getAttribute('x')) +
-            (parseFloat(informationBox.getAttribute('width')/ 2) -
+            (parseFloat(informationBox.getAttribute('width') / 2) -
                 (parseFloat(readMoreButton.getAttribute('width')) / 2)));
     readMoreButton.setAttribute('y', 
         (parseFloat(informationBox.getAttribute('y')) +
             (parseFloat(informationBox.getAttribute('height')) -
                 (parseFloat(readMoreButton.getAttribute('height')))) - 5));
-    readMoreButton.setAttribute('opacity', '0');
     readMoreButton.removeAttribute('transform');
 
     const readMoreLink = document.createElementNS(svgns, 'a');
     readMoreLink.setAttribute('href', linkAdress);
     readMoreLink.append(readMoreButton);
 
-    readMoreButton.classList.add('fade-in');
-    informationBox.classList.add('fade-in');
-
     infinityInformationLayer.append(informationBox);
     infinityInformationLayer.append(readMoreLink);
+    infinityInformationLayer.classList.add('fade-in');
 
     return infinityInformationLayer;
 }
